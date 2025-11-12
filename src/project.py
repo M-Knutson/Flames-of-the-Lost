03e4e85ff4cb69c9b47b6f95cd9a2d37a68b173e
M Knutson <dal624043@utdallas.edu>
2025-11-12 16:29:16 -0600

    new code: while loop for main event loop & quit code

--- src/project.py
+++ src/project.py
@@ -5,16 +5,26 @@ def main():
     pygame.init()
     pygame.display.set_caption("Flames of the Lost")
     clock = pygame.time.Clock()
-    dt = 0
 
     os.environ['SDL_VIDEOCENTERED'] = '1'
     info = pygame.display.Info()
     monitor_width, monitor_height = info.current_w, info.current_h
-
-    resolution = (monitor_width, monitor_height)
+    resolution = (monitor_width - 50, monitor_height - 50)
     screen = pygame.display.set_mode(resolution)
 
     running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+
+        green = pygame.Color(82, 179, 143)
+        screen.fill(green)
+        pygame.display.flip()
+        clock.tick(60)
+
 
 
 
