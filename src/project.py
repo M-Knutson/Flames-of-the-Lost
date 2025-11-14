1002af3ef887da027d56ab7d664f8480c93a151d
M Knutson <dal624043@utdallas.edu>
2025-11-13 18:08:33 -0600

    new code: class Player & blit player

--- src/project.py
+++ src/project.py
@@ -1,6 +1,17 @@
 import os
 import pygame
 
+class Player():
+
+    def __init__(self, character, pos = (0,0), size = (50, 50), speed = 5):
+        self.character = character
+        self.pos = pos
+        self.size = size
+        self.speed = speed
+        
+    def update(self):
+        pass
+
 def main():
     pygame.init()
     pygame.display.set_caption("Flames of the Lost")
@@ -12,6 +23,13 @@ def main():
     resolution = (monitor_width - 50, monitor_height - 50)
     screen = pygame.display.set_mode(resolution)
 
+    player_x = monitor_width // 2
+    player_y = monitor_height // 2
+    player_size = 50
+    character = pygame.Surface((player_size, player_size))
+    character.fill((219, 129, 96))
+    player = Player(character, pos=(player_x, player_y), size=player_size)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -22,6 +40,8 @@ def main():
 
         green = pygame.Color(82, 179, 143)
         screen.fill(green)
+        player.update
+        screen.blit(player.character, (player_x, player_y))
         pygame.display.flip()
         clock.tick(60)
 
